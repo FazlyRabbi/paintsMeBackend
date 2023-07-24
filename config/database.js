cd97b34ec7715cbef941531d864ad2c2239428df
FazlyRabbi <rabbisam74@gmail.com>
2023-07-24 22:14:05 +0600

    chnage db

--- config/database.js
+++ config/database.js
@@ -1,15 +1,16 @@
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cgc15r82qv267uaq5h90-a.singapore-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'kingdomofkush'),
-      user: env('DATABASE_USERNAME', 'kingdomofkush_user'),
-      password: env('DATABASE_PASSWORD', 'kingdomofkush_user'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env(
+        "DATABASE_HOST",
+        "dpg-civ98t15rnuhcnrkm93g-a.oregon-postgres.render.com"
+      ),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "printandgraph_3pd6"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "b9qGSkW9bz80YB2RlFkapISbiZWDYDiP"),
+      ssl: env.bool("DATABASE_SSL", true),
     },
   },
 });
-
